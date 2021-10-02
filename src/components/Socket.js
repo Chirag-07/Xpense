9b6dd8d512b1495a1f31220a61130b751b788f45
Chirag <chiragk927@gmail.com>
2021-10-01 23:53:14 -0400

    change ENDPOINT in socket.js

--- src/components/Socket.js
+++ src/components/Socket.js
@@ -1,5 +1,6 @@
 import io from 'socket.io-client';
 
-const ENDPOINT = 'https://xpense-server.herokuapp.com/';
+// const ENDPOINT = 'https://xpense-server.herokuapp.com/';
+const ENDPOINT = 'https://xpense-project-server.herokuapp.com/';
 
 export const socket = io(ENDPOINT);
